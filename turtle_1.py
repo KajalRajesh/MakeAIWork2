eb1ff2c43bbd95b4dcd18c4277a89f2f9e8a70f4
KajalRajesh <mail2kajalr@gmail.com>
2023-05-08 14:00:56 +0200

    turtle

---
+++ turtle_1.py
@@ -0,0 +1,33 @@
+import turtle
+
+
+t = turtle.Turtle()
+t.getscreen().bgcolor("black" )
+t.shape("turtle")
+#t.shape("arrow")
+t.color("red","yellow")
+t.speed(79)
+t.circle(60)
+t.dot(20)
+t.right(90)
+
+t.forward(100)
+
+t.left(90)
+
+t.backward(100)
+
+# t.begin_fill()
+# t.fd(100)
+# t.lt(120)
+# t.fd(100)
+# t.lt(120)
+# t.fd(100)
+# t.end_fill()
+#t.shape("circle")
+
+
+
+
+input("press Enter to exit")
+
